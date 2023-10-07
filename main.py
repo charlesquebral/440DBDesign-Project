aa5c7718c465e8f1638fbcf8017315ab6a3bbb69
charlesquebral <139582402+bpc-caquebral@users.noreply.github.com>
2023-10-07 09:47:40 -0700

    Initialized Repo

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask, redirect, url_for, render_template, session
+
+app = Flask(__name__)
+
+@app.route('/')
+def base():
+    return redirect(url_for('login'))
+
+@app.route('/login', methods=['POST','GET'])
+def login():
+    error= None
+    return render_template('login.html', error=error)
+
+@app.route('/signup', methods=['POST','GET'])
+def signup():
+    error= None
+    return render_template('signup.html', error=error)
+
+if __name__ == "__main__":
+    app.run(debug=1, host='0.0.0.0', port=5000)
